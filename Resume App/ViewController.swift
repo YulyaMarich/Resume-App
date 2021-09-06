380e5bc88df3b22e20e88ef0221a28658cb38c8c
Yulia Marych <yulyamarich@icloud.com>
2021-09-06 19:41:13 +0300

    Initial commit

---
+++ Resume App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resume App
+//
+//  Created by Julia on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
